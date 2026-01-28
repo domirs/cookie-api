26ea00b5ac41dcc4a028948b7729f4ce4a0ac6c0
Dominik Forster <dominik.forster@axa.ch>
2026-01-28 14:09:33 +0100

    feat: :tada: init

--- src/login.controller.ts
+++ src/login.controller.ts
@@ -17,6 +17,7 @@ export class LoginController {
   setCookie(@Body() body: { name: string }, @Res() res: Response): void {
     res.cookie('username', 'world', {
       httpOnly: true,
+      secure: true,
       sameSite: 'none',
       maxAge: 3600000,
       domain: 'cookie-api-2slo.onrender.com',
