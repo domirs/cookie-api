acb0d803162b64549177e7ddc493e5645d2590c8
Dominik Forster <dominik.forster@axa.ch>
2026-01-28 09:22:54 +0100

    feat: :tada: init

---
+++ src/login.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import type { Request, Response } from 'express';
+import { LoginService } from './login.service';
+
+@Controller('/login')
+export class LoginController {
+  constructor(private readonly loginService: LoginService) {}
+
+  @Get()
+  getCookie(@Req() request: Request): string {
+    console.log(request.cookies); // or "request.cookies['cookieKey']"
+
+    return `Hello, ${request.cookies['username']}`;
+  }
+
+  @Post()
+  setCookie(@Body() body: { name: string }, @Res() res: Response): void {
+    res.cookie('username', 'world', { httpOnly: true });
+    res.send({ success: true });
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import { LoginModule } from './login.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(LoginModule);
+
+  app.enableCors();
+  app.use(cookieParser('secret'));
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+void bootstrap();
